41c069929f81cd2d66a8e6191e0fd256c6b0752f
Slavcho Mitrov <mitrov.slavcho@gmail.com>
2025-12-12 09:08:28 +0100

    create the logic for downloading the pdf (controller method and view)

--- routes/web.php
+++ routes/web.php
@@ -4,4 +4,4 @@ use App\Http\Controllers\CvController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', [CvController::class, 'show'])->name('cv.show');
-Route::post('/download', [CvController::class, 'downloadPdf'])->name('cv.download');
+Route::get('/download', [CvController::class, 'downloadPdf'])->name('cv.download');
